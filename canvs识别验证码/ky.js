1f88e5edf5422a0f5d18d5cc159ba3f2c4c956cb
hqlkylin <qilin1011@sohu.com>
2015-09-28 14:49:32 +0800

    1.0

--- canvs识别验证码/ky.js
+++ canvs识别验证码/ky.js
@@ -8,6 +8,7 @@ startime = 1;
 var $body = $("body");
 
 var iframe = $("<iframe>").width(1000).height(500).css({"position": "absolute", top: 0, left: 0})
+
     .attr("src", "http://www.jimei.cn/front/order/saveorder.html?pid=d5da89b541384243a2f2c0650fe09ad6&activityProductId=5ac425aed3634118901af9c53c2432b5&code=");
 $body.append(iframe)
 
