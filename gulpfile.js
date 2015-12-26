d8591cf63b456332c892219403891abb4fe4002d
hanqilin <hqlkylin@gmail.com>
2015-12-26 23:56:37 +0800

    添加gulp 自动化构建, 目前只针对js检查有效

---
+++ gulpfile.js
@@ -0,0 +1,107 @@
+/**
+ * Created by hanqilin on 15/12/26.
+ * npm install gulp-util gulp-imagemin gulp-ruby-sass gulp-minify-css gulp-jshint gulp-uglify gulp-rename gulp-concat gulp-clean  --save-dev
+ */
+
+// 引入 gulp及组件
+var gulp    = require('gulp'),                 //基础库
+    imagemin = require('gulp-imagemin'),       //图片压缩
+    sass = require('gulp-ruby-sass'),          //sass
+    minifycss = require('gulp-minify-css'),    //css压缩
+    jshint = require('gulp-jshint'),           //js检查
+    uglify  = require('gulp-uglify'),          //js压缩
+    rename = require('gulp-rename'),           //重命名
+    concat  = require('gulp-concat'),          //合并文件
+    clean = require('gulp-clean');             //清空文件夹
+
+// HTML处理
+//gulp.task('html', function() {
+//    var htmlSrc = './src/*.html',
+//        htmlDst = './dist/';
+//
+//    gulp.src(htmlSrc)
+//        .pipe(livereload(server))
+//        .pipe(gulp.dest(htmlDst))
+//});
+
+// 样式处理
+//gulp.task('css', function () {
+//    var cssSrc = './src/scss/*.scss',
+//        cssDst = './dist/css';
+//
+//    gulp.src(cssSrc)
+//        .pipe(sass({ style: 'expanded'}))
+//        .pipe(gulp.dest(cssDst))
+//        .pipe(rename({ suffix: '.min' }))
+//        .pipe(minifycss())
+//        .pipe(livereload(server))
+//        .pipe(gulp.dest(cssDst));
+//});
+
+// 图片处理
+//gulp.task('images', function(){
+//    var imgSrc = './src/images/**/*',
+//        imgDst = './dist/images';
+//    gulp.src(imgSrc)
+//        .pipe(imagemin())
+//        .pipe(livereload(server))
+//        .pipe(gulp.dest(imgDst));
+//})
+
+// js处理
+gulp.task('js', function () {
+    var jsSrc = './h5/**/*.js',
+        jsDst ='./dist/js';
+
+    gulp.src(jsSrc)
+        .pipe(jshint('.jshintrc'))
+        .pipe(jshint.reporter('default'))
+        //.pipe(concat('main.js'))
+        //.pipe(gulp.dest(jsDst))
+        //.pipe(rename({ suffix: '.min' }))
+        //.pipe(uglify())
+        //.pipe(livereload(server))
+        //.pipe(gulp.dest(jsDst));
+});
+
+// 清空图片、样式、js
+gulp.task('clean', function() {
+    gulp.src(['./dist/css', './dist/js', './dist/images'], {read: false})
+        .pipe(clean());
+});
+
+// 默认任务 清空图片、样式、js并重建 运行语句 gulp
+gulp.task('default', function(){
+    gulp.start('js');
+    //gulp.start('html','css','images','js');
+});
+
+// 监听任务 运行语句 gulp watch
+gulp.task('watch',function(){
+
+
+        //if (err) {
+        //    return console.log(err);
+        //}
+        //
+        //// 监听html
+        //gulp.watch('./src/*.html', function(event){
+        //    gulp.run('html');
+        //})
+        //
+        //// 监听css
+        //gulp.watch('./src/scss/*.scss', function(){
+        //    gulp.run('css');
+        //});
+        //
+        //// 监听images
+        //gulp.watch('./src/images/**/*', function(){
+        //    gulp.run('images');
+        //});
+
+        // 监听js
+        gulp.watch('./h5/**/*.js', function(){
+            gulp.run('js');
+        });
+
+});
---
+++ h5/simpleDemo/js/index.js
@@ -0,0 +1,8 @@
+/**
+ * Created by hanqilin on 15/12/26.
+ */
+
+var a=0;
+console.log(1);
+console.log(1);
+
