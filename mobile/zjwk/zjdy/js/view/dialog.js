623d982df3fe2bfab582f7a4d1fa979c89fdde1a
hanqilin <hanqilin@fingercrm.cn>
2016-02-17 10:28:32 +0800

    合并

--- mobile/zjwk/zjdy/js/view/dialog.js
+++ mobile/zjwk/zjdy/js/view/dialog.js
@@ -1,6 +1,6 @@
 /**
  * Created by hanqilin on 16/2/15.
- */
+*/
 
 var Dialog = function (opt) {
 
@@ -10,26 +10,68 @@ var Dialog = function (opt) {
         button: ['确认'],
         select: 0,
         allowScroll: false
-    }
+    };
+
     $.extend(this.opt, opt);
+    this.renderHtml();
+    this.button = this.element.find('.weui_btn_dialog');
+    this._bindEvent();
+    this.toggle();
 };
 Dialog.prototype = {
     renderHtml: function () {
+
         // 默认模板
-        var _dialogTpl = '<div class="ui-dialog">' +
-            '<div class="ui-dialog-cnt">' +
-            '<div class="ui-dialog-bd">' +
-            '<div>' +
-            '<h4>' + this.opt.title + '</h4>' +
-            '<div>' + this.opt.content + '</div></div>' +
-            '</div>' +
-            '<div class="ui-dialog-ft ui-btn-group">';
+        var _dialog = '<div class="weui_dialog_confirm">' +
+            '<div class="weui_mask"></div>' +
+            '<div class="weui_dialog">' +
+            '<div class="weui_dialog_hd"><strong class="weui_dialog_title">' + this.opt.title + '</strong></div>' +
+            '<div class="weui_dialog_bd">' + this.opt.content + '</div>' +
+            '<div class="weui_dialog_ft">';
+
         for (var i = 0; i < this.opt.button.length; i++) {
-            _dialogTpl += '<button type="button" data-role="button"  class="select" id="dialogButton' + i + '">' +
-                this.opt.button[i] + '</button>';
+            _dialog += '<a href="javascript:;" class="weui_btn_dialog default">' +
+                this.opt.button[i] + '</a>'
         }
-        _dialogTpl += '</div>' +
+        _dialog += '</div>' +
             '</div>' +
             '</div>';
+
+        this.element = $(_dialog).appendTo($('body'));
+
+    },
+    _bindEvent: function () {
+        var self = this;
+        self.button.on("tap", function () {
+            var index = $(self.button).index($(this));
+            // self.option.callback("button",index);
+            var e = $.Event("dialog:action");
+            e.index = index;
+            self.element.trigger(e);
+            self.hide.apply(self);
+            return false;
+        });
+    },
+    show: function () {
+
+        var self = this;
+        self.element.trigger($.Event("dialog:show"));
+        self.element.addClass("active");
+
+    },
+    hide: function () {
+        var self = this;
+        self.element.trigger($.Event("dialog:hide"));
+        self.element.removeClass("active");
+        self.element.remove();
+
+    },
+    toggle: function () {
+        if (this.element.hasClass("active")) {
+            this.hide();
+        } else {
+            this.show();
+        }
     }
+
 };
--- mobile/zjwk/zjhy/js/view/plugins.js
+++ mobile/zjwk/zjhy/js/view/plugins.js
@@ -13,28 +13,27 @@ $.wx_hideTipData = function () {
     }, 20);
 };
 /*******************************************************************************************************
- *  调用方法 : $.wx_popTips(options)    头部 浮层提示条
- *
- *  var pop=$.popTips({
- *       "content": "恭喜您 操作成功!",
- *       "type": "info",
- *       "stayTime": 2000
- *   });
- *   自定义事件
- *  pop.on("tips:show", function () {
- *               console.log("sb");
- *           })
- *  pop.on("tips:hide", function () {
- *               console.log("bs");
- *          })
+ *  调用方法 : $.popTips(options)    头部 浮层提示条
  ********************************************************************************************************/
+/*var pop = $.popTips({
+ "content": "恭喜您 操作成功!",
+ "type": "info",
+ "stayTime": 2000
+ });
+ //自定义事件
+ pop.on("tips:show", function () {
+ console.log("sb");
+ });
+ pop.on("tips:hide", function () {
+ console.log("bs");
+ });*/
 $.popTips = function (options) {
 
     var PopTips = function (options) {
         var self = this;
         // 默认参数
         this.opts = {
-            content: '',
+            content: '成功',
             stayTime: 2000,
             type: 'info'
         };
@@ -91,6 +90,102 @@ $.popTips = function (options) {
     return new PopTips(options).element;
 };
 
+/*******************************************************************************************************
+ *  调用方法 : $.dialog(options)    弹出框
+ ********************************************************************************************************/
+/*
+ var dia = $.dialog({
+ title: '温馨提示',
+ content: '温馨提示内容',
+ button: ["取消", "确认"]
+ });
+ dia.on("dialog:action", function (e) {  //e.index 点击 按钮的索引
+ if (e.index == 0) {
+ $.popTips({
+ "content": "您点击了取消按钮！",
+ "type": "info",
+ "stayTime": 2000
+ });
+ } else {
+ $.toast("您点击了确定按钮！")
+ }
+ });
+ dia.on("dialog:hide", function (e) {
+ console.log("dialog hide")
+ });
+ dia.on("dialog:show", function (e) {
+ console.log("dialog hide")
+ });*/
+
+$.dialog = function (options) {
+    var Dialog = function (opt) {
+        this.opt = {
+            title: '',
+            content: '',
+            button: ['确认'],
+            select: 0,
+            allowScroll: false
+        };
+        $.extend(this.opt, opt);
+        this.renderHtml();
+        this.button = this.element.find('.weui_btn_dialog');
+        this._bindEvent();
+        this.toggle();
+    };
+    Dialog.prototype = {
+        renderHtml: function () {
+            // 默认模板
+            var _dialog = '<div class="weui_dialog_confirm">' +
+                '<div class="weui_mask"></div>' +
+                '<div class="weui_dialog">' +
+                '<div class="weui_dialog_hd"><strong class="weui_dialog_title">' + this.opt.title + '</strong></div>' +
+                '<div class="weui_dialog_bd">' + this.opt.content + '</div>' +
+                '<div class="weui_dialog_ft">';
+            for (var i = 0; i < this.opt.button.length; i++) {
+                _dialog += '<a href="javascript:;" class="weui_btn_dialog default">' +
+                    this.opt.button[i] + '</a>'
+            }
+            _dialog += '</div>' +
+                '</div>' +
+                '</div>';
+            this.element = $(_dialog).appendTo($('body'));
+        },
+        _bindEvent: function () {
+            var self = this;
+            self.button.on("tap", function () {
+                var index = $(self.button).index($(this));
+                // self.option.callback("button",index);
+                var e = $.Event("dialog:action");
+                e.index = index;
+                self.element.trigger(e);
+                self.hide.apply(self);
+                return false;
+            });
+        },
+        show: function () {
+
+            var self = this;
+            self.element.trigger($.Event("dialog:show"));
+            self.element.addClass("active");
+
+        },
+        hide: function () {
+            var self = this;
+            self.element.trigger($.Event("dialog:hide"));
+            self.element.removeClass("active");
+            self.element.remove();
+
+        },
+        toggle: function () {
+            if (this.element.hasClass("active")) {
+                this.hide();
+            } else {
+                this.show();
+            }
+        }
+    };
+    return new Dialog(options).element;
+};
 
 /*******************************************************************************************************
  *  调用方法 : $.toast(msg,type)    自动消失提示框
@@ -135,9 +230,10 @@ $.ajaxJson = function (url, data, onSuccess, onError) {
 };
 /*---------------------------------------- Ajax 扩展 -------------------------------------------------*/
 
+
+
+
 $(function () {
-
-
     /* 解决iOS下input和fixed 问题*/
     !function () {
         var $box = $(".ft");
@@ -154,5 +250,4 @@ $(function () {
             })
         }
     }();
-
 });
