0bd086c133531c2c0ce032d4ebf17623da7a877f
hanqilin <hqlkylin@gmail.com>
2016-05-19 19:09:09 +0800

    页面模版

---
+++ zjwk/inspinia_admin-master/js/plugins/custombox/custombox.core.js
@@ -0,0 +1,38 @@
+
+
+/**
+ * Components
+ */
+!function($) {
+    "use strict";
+    var Components = function() {};
+    //initializing custom modal
+    Components.prototype.initCustomModalPlugin = function() {
+        $('[data-plugin="custommodal"]').on('click', function(e) {
+        	Custombox.open({
+                target: $(this).attr("href"),
+                effect: $(this).attr("data-animation"),
+                overlaySpeed: $(this).attr("data-overlaySpeed"),
+                overlayColor: $(this).attr("data-overlayColor")
+            });
+        	e.preventDefault();
+        });
+    }
+    //initilizing
+    Components.prototype.init = function() {
+        var $this = this;
+        this.initCustomModalPlugin()
+    },
+
+    $.Components = new Components, $.Components.Constructor = Components
+
+}(window.jQuery),
+    //initializing main application module
+function($) {
+    "use strict";
+    $.Components.init();
+}(window.jQuery);
+
+
+
+
